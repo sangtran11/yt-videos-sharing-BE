95abab0d0299fb3d6551b20e66808a1d2be39190
SangTran <sang.tran@adurolife.com>
2022-05-06 17:32:31 +0700

    change db url

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use("/user", usersRoutes);
 
 const CONNECTION_URL =
-  "mongodb+srv://movies_db_dev:admin@cluster0.p5lcd.mongodb.net/moviesDB?retryWrites=true&w=majority";
+  "mongodb+srv://movies_db_dev:admin@develop-db.p5lcd.mongodb.net/moviesDB?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
 mongoose
   .connect(CONNECTION_URL)
